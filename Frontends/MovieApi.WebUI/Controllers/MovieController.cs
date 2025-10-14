3dfe9041d150400932ef1c944460675d62d5c3e4
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2025-10-15 02:43:02 +0300

    Ders 28

--- Frontends/MovieApi.WebUI/Controllers/MovieController.cs
+++ Frontends/MovieApi.WebUI/Controllers/MovieController.cs
@@ -1,14 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.MovieDtos;
+using Newtonsoft.Json;
 
 namespace MovieApi.WebUI.Controllers;
 
 public class MovieController : Controller
 {
-    public IActionResult MovieList()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public MovieController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> MovieList()
     {
         ViewBag.v1 = "Film Listesi";
         ViewBag.v2 = "Ana Sayfa";
         ViewBag.v3 = "Tüm Filmler";
+
+        var client = _httpClientFactory.CreateClient();
+
+        var responseMessage = await client.GetAsync("https://localhost:7031/api/Movies");
+
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            List<ResultMovieDto>? values = await responseMessage.Content.ReadFromJsonAsync<List<ResultMovieDto>>();
+
+            return View(values);
+        }
+
         return View();
     }
 }
