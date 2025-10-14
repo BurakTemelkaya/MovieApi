47304ab6662840010dd7e1c963811e70f0936806
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2025-10-14 22:30:12 +0300

    Ders 23

---
+++ Frontends/MovieApi.WebUI/Controllers/UserWebUILayoutController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Controllers;
+
+public class UserWebUILayoutController : Controller
+{
+    public IActionResult LayoutUI()
+    {
+        return View();
+    }
+}
