30becd84bdb39bbd77d9251629c6d01e1a32d2cd
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2025-10-16 00:31:07 +0300

    Ders 34 tamamlandı.

---
+++ Infrastructure/MovieApi.Persistence/Identity/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MovieApi.Persistence.Identity;
+
+public class AppUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string? ImageUrl { get; set; }
+}
