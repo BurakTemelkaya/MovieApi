4a324f02b174ddd1544d5a7e05fea6561eb9194d
Burak Temelkaya <temelkayaburak@gmail.com>
2025-12-30 06:04:10 +0300

    Ders 44 tamamlandı.

---
+++ Frontends/MovieApi.Dto/Dtos/AdminCategoryDtos/AdminResultCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieApi.Dto.Dtos.AdminCategoryDtos;
+
+public class AdminResultCategoryDto
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public int MovieCount { get; set; }
+    public int ReviewCount { get; set; }
+    public int AvgRating { get; set; }
+    public bool IsActive { get; set; }
+}
---
+++ Frontends/MovieApi.WebUI/Areas/Admin/Controllers/AdminCategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.AdminMovieDtos;
+
+namespace MovieApi.WebUI.Areas.Admin.Controllers;
+
+public class AdminCategoryController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public AdminCategoryController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> CategoryList()
+    {
+        var client = _httpClientFactory.CreateClient();
+
+        var responseMessage = await client.GetAsync("https://localhost:7031/api/Categories");
+
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            List<AdminResultMovieDto>? values = await responseMessage.Content.ReadFromJsonAsync<List<AdminResultMovieDto>>();
+
+            return View(values);
+        }
+
+        return View();
+    }
+}
