aa6efc552b12283a6994507fa2c4d8a1a7b85ba1
Burak Temelkaya <temelkayaburak@gmail.com>
2026-01-03 01:39:30 +0300

    Ders 47 tamamlandı.

--- Core/MovieApi.Domain/Entities/Category.cs
+++ Core/MovieApi.Domain/Entities/Category.cs
@@ -4,4 +4,13 @@ public class Category
 {
     public int CategoryId { get; set; }
     public string CategoryName { get; set; }
+    public bool Status { get; set; }
+    public string? Description { get; set; }
+    public ICollection<Movie> Movies { get; set; }
+
+    public Category()
+    {
+        CategoryName = string.Empty;
+        Movies = new HashSet<Movie>();
+    }
 }
